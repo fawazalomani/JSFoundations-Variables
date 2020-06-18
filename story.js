7f0d8d82163dba60d009deef7e6f9d66c4c9ec26
fawaz <st0ock@hotmail.com>
2020-06-18 13:47:53 +0300

    new practice

--- story.js
+++ story.js
@@ -1,12 +1,22 @@
 // Define the name here on line 2
-let name = 'fawaz'
+
+
+const username = "fawaz";
+
+
 // Define the age here on line 4
-let age = 40
+
+const age = 40;
+
 // Define the hobby here on line 6
-let hobby = 'trading'
+
+const hobby = "trading";            
+
 // Print the story here on line 8
-onsole.log(`Mr.${name}, in 33 years, you will be age ${age + 33}. By then you will have decades of ${hobby} practice`
-  );
+
+console.log(` Mr. ${username}, in 33 years, you will be age ${age + 33}, years old. By then you will have decade of ${hobby} practice `);
+
+
   
   
 
